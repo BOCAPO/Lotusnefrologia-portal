e521b55507d6933e92369b498257e92341cba954
Danilo Alves <33788984+danilofas@users.noreply.github.com>
2023-07-30 18:23:45 -0300

    menus adm

---
+++ src/tests/mocks/dataPatients.js
@@ -0,0 +1,139 @@
+const dataPatients = [
+  [
+    'Ana Silva',
+    '123.456.789-10',
+    'Unidade 1',
+    '10/05/1980',
+    '(11) 99999-1234',
+    'ana.silva@email.com',
+    'Ativo'
+  ],
+  [
+    'Carlos Santos',
+    '987.654.321-98',
+    'Unidade 2',
+    '15/08/1975',
+    '(11) 99999-5678',
+    'carlos.santos@email.com',
+    'Bloqueado'
+  ],
+  [
+    'Mariana Almeida',
+    '345.678.901-23',
+    'Unidade 3',
+    '25/11/1990',
+    '(11) 99999-9876',
+    'mariana.almeida@email.com',
+    'Ativo'
+  ],
+  [
+    'Pedro Souza',
+    '654.321.098-76',
+    'Unidade 2',
+    '05/03/1985',
+    '(11) 99999-2345',
+    'pedro.souza@email.com',
+    'Ativo'
+  ],
+  [
+    'Joana Oliveira',
+    '789.012.345-67',
+    'Unidade 1',
+    '12/07/1978',
+    '(11) 99999-5678',
+    'joana.oliveira@email.com',
+    'Bloqueado'
+  ],
+  [
+    'Rafael Mendes',
+    '234.567.890-12',
+    'Unidade 3',
+    '28/02/1992',
+    '(11) 99999-7890',
+    'rafael.mendes@email.com',
+    'Ativo'
+  ],
+  [
+    'Laura Lima',
+    '567.890.123-45',
+    'Unidade 2',
+    '03/09/1982',
+    '(11) 99999-9012',
+    'laura.lima@email.com',
+    'Ativo'
+  ],
+  [
+    'Fernando Costa',
+    '901.234.567-89',
+    'Unidade 1',
+    '18/11/1973',
+    '(11) 99999-2345',
+    'fernando.costa@email.com',
+    'Bloqueado'
+  ],
+  [
+    'Paula Santos',
+    '123.456.789-10',
+    'Unidade 3',
+    '10/05/1987',
+    '(11) 99999-5678',
+    'paula.santos@email.com',
+    'Ativo'
+  ],
+  [
+    'José Silva',
+    '987.654.321-98',
+    'Unidade 2',
+    '15/08/1981',
+    '(11) 99999-9876',
+    'jose.silva@email.com',
+    'Bloqueado'
+  ],
+  [
+    'Camila Almeida',
+    '345.678.901-23',
+    'Unidade 1',
+    '25/11/1976',
+    '(11) 99999-2345',
+    'camila.almeida@email.com',
+    'Ativo'
+  ],
+  [
+    'Lucas Souza',
+    '654.321.098-76',
+    'Unidade 3',
+    '05/03/1995',
+    '(11) 99999-5678',
+    'lucas.souza@email.com',
+    'Ativo'
+  ],
+  [
+    'Juliana Oliveira',
+    '789.012.345-67',
+    'Unidade 2',
+    '12/07/1998',
+    '(11) 99999-9012',
+    'juliana.oliveira@email.com',
+    'Bloqueado'
+  ],
+  [
+    'Gustavo Mendes',
+    '234.567.890-12',
+    'Unidade 1',
+    '28/02/1989',
+    '(11) 99999-2345',
+    'gustavo.mendes@email.com',
+    'Ativo'
+  ],
+  [
+    'Amanda Lima',
+    '567.890.123-45',
+    'Unidade 2',
+    '03/09/1984',
+    '(11) 99999-5678',
+    'amanda.lima@email.com',
+    'Ativo'
+  ]
+];
+
+export default dataPatients;
---
+++ src/tests/mocks/dataSpecialist.js
@@ -0,0 +1,130 @@
+const dataSpecialist = [
+  [
+    'Ana Silva',
+    '123.456.789-10',
+    'ana.silva',
+    '(11) 99999-1234',
+    'ana.silva@email.com',
+    'Geriatria',
+    'Ativo'
+  ],
+  [
+    'Carlos Santos',
+    '987.654.321-98',
+    'carlos.santos',
+    '(11) 99999-5678',
+    'carlos.santos@email.com',
+    'Cirurgião Geral',
+    'Bloqueado'
+  ],
+  [
+    'Mariana Almeida',
+    '345.678.901-23',
+    'mariana.almeida',
+    '(11) 99999-9876',
+    'mariana.almeida@email.com',
+    'Nefrologia',
+    'Ativo'
+  ],
+  [
+    'Pedro Souza',
+    '654.321.098-76',
+    'pedro.souza',
+    '(11) 99999-2345',
+    'pedro.souza@email.com',
+    'Geriatria',
+    'Ativo'
+  ],
+  [
+    'Mariana Almeida',
+    '345.678.901-23',
+    'mariana.almeida',
+    '(11) 99999-9876',
+    'mariana.almeida@email.com',
+    'Nefrologia',
+    'Ativo'
+  ],
+  [
+    'Pedro Souza',
+    '654.321.098-76',
+    'pedro.souza',
+    '(11) 99999-2345',
+    'pedro.souza@email.com',
+    'Geriatria',
+    'Ativo'
+  ],
+  [
+    'Mariana Almeida',
+    '345.678.901-23',
+    'mariana.almeida',
+    '(11) 99999-9876',
+    'mariana.almeida@email.com',
+    'Nefrologia',
+    'Ativo'
+  ],
+  [
+    'Pedro Souza',
+    '654.321.098-76',
+    'pedro.souza',
+    '(11) 99999-2345',
+    'pedro.souza@email.com',
+    'Geriatria',
+    'Ativo'
+  ],
+  [
+    'Mariana Almeida',
+    '345.678.901-23',
+    'mariana.almeida',
+    '(11) 99999-9876',
+    'mariana.almeida@email.com',
+    'Nefrologia',
+    'Ativo'
+  ],
+  [
+    'Pedro Souza',
+    '654.321.098-76',
+    'pedro.souza',
+    '(11) 99999-2345',
+    'pedro.souza@email.com',
+    'Geriatria',
+    'Ativo'
+  ],
+  [
+    'Mariana Almeida',
+    '345.678.901-23',
+    'mariana.almeida',
+    '(11) 99999-9876',
+    'mariana.almeida@email.com',
+    'Nefrologia',
+    'Ativo'
+  ],
+  [
+    'Pedro Souza',
+    '654.321.098-76',
+    'pedro.souza',
+    '(11) 99999-2345',
+    'pedro.souza@email.com',
+    'Geriatria',
+    'Ativo'
+  ],
+  [
+    'Mariana Almeida',
+    '345.678.901-23',
+    'mariana.almeida',
+    '(11) 99999-9876',
+    'mariana.almeida@email.com',
+    'Nefrologia',
+    'Ativo'
+  ],
+  [
+    'Pedro Souza',
+    '654.321.098-76',
+    'pedro.souza',
+    '(11) 99999-2345',
+    'pedro.souza@email.com',
+    'Geriatria',
+    'Ativo'
+  ]
+];
+
+export default dataSpecialist;
