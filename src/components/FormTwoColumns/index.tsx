da68b9ab48825535d199208e896175988aeb702e
Danilo Alves <33788984+danilofas@users.noreply.github.com>
2023-08-11 18:36:14 -0300

    ajustes escala de consultas

--- src/components/FormTwoColumns/index.tsx
+++ src/components/FormTwoColumns/index.tsx
@@ -3,12 +3,14 @@
 import React from 'react';
 
 import { Icon, TypeIcon } from 'components/Icone';
+import { SpinnerLoading } from 'components/Spinner';
 import { SmallMediumText } from 'components/Text';
 
 import styles from './formtwocolumns.module.css';
 
 import { Strings } from 'assets/Strings';
 import { Colors } from 'configs/Colors_default';
+import { DataSpecialistsModel } from 'models/DataSpecialistsModel';
 import { ResponseGetModel } from 'models/ResponseGetModel';
 
 type Props = {
@@ -18,16 +20,17 @@ type Props = {
   style?: React.CSSProperties;
   isLoading?: boolean;
   onItemClick?: (_item: any) => void;
+  type?: string;
 };
 
 export function FormTwoColumns({
   headers,
-  headersResponse,
   response,
   isLoading = true,
+  type,
   onItemClick
 }: Props): JSX.Element {
-  const [keys, setKeys] = React.useState<string[] | undefined>([]);
+  const [dataAdapted, setDataAdapted] = React.useState<any[]>([]);
   function handleRowClick(event: React.MouseEvent<HTMLTableRowElement>) {
     const userId = event.currentTarget.getAttribute('data-user-id');
     if (userId !== undefined && onItemClick !== undefined) {
@@ -40,13 +43,38 @@ export function FormTwoColumns({
       response?.data !== null &&
       response?.data.length > 0
     ) {
-      const objectKeys: string[] = Object.keys(response?.data[0]);
-      const commonKeys = headersResponse?.filter(
-        (key) => objectKeys?.includes(key)
-      );
-      setKeys(commonKeys);
+      const links = response.links;
+      links.map((link) => {
+        if (
+          link.label === '&laquo; Anterior' ||
+          link.label === 'Próximo &raquo;'
+        ) {
+          links.splice(links.indexOf(link), 1);
+        }
+      });
+      switch (type) {
+        case 'scaleSchedule':
+          adptedDataSpecialist(response?.data);
+          break;
+
+        default:
+          break;
+      }
     }
   }, [response?.data?.length]);
+
+  async function adptedDataSpecialist(data: any) {
+    data.forEach((element: DataSpecialistsModel) => {
+      element.units.forEach((unit) => {
+        const dataAdapted = {
+          id: element.id,
+          firstColumn: element.name,
+          secondColumn: unit.name
+        };
+        setDataAdapted((oldArray) => [...oldArray, dataAdapted]);
+      });
+    });
+  }
 
   return (
     <div className={styles.bodyFormTwoColumns}>
@@ -66,60 +94,51 @@ export function FormTwoColumns({
         </div>
       </div>
       <div className={styles.containerTable}>
-        <table className={styles.tableScale}>
-          <thead>
-            <tr className={styles.headers}>
-              {headers.map((header, index) => (
-                <th key={index}>{header}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {response?.data.map((row: any, index: number) => (
-              <tr key={index} onClick={handleRowClick} data-user-id={row.id}>
-                {keys?.map((key, index) => (
-                  <td key={index} className={styles.rows}>
-                    {key === 'status' ? (
-                      row[key] === 0 ? (
-                        <span>Ativo</span>
-                      ) : (
-                        <span>Inativo</span>
-                      )
-                    ) : row[key] === null ? (
-                      'Não informado'
-                    ) : key === 'birthday' ? (
-                      new Date(row[key]).toLocaleDateString('pt-BR')
-                    ) : (
-                      row[key].toString()
-                    )}
-                  </td>
+        {isLoading ? (
+          <SpinnerLoading />
+        ) : (
+          <table className={styles.tableScale}>
+            <thead>
+              <tr className={styles.headers}>
+                {headers.map((header, index) => (
+                  <th key={index}>{header}</th>
                 ))}
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {dataAdapted.map((row: any, index: number) => (
+                <tr key={index} onClick={handleRowClick} data-user-id={row.id}>
+                  <td className={styles.rows}>{row.firstColumn}</td>
+                  <td className={styles.rows}>{row.secondColumn}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
       </div>
-      <div className={styles.footer}>
-        <div className={styles.footerLeft}>
-          <select>
-            {response?.links.map((link, index) => (
-              <option key={index}>{link?.label}</option>
-            ))}
-          </select>
+      {!isLoading ? (
+        <div className={styles.footer}>
+          <div className={styles.footerLeft}>
+            <select>
+              {response?.links.map((link, index) => (
+                <option key={index}>{link?.label}</option>
+              ))}
+            </select>
+          </div>
+          <SmallMediumText
+            bold={false}
+            style={{ lineHeight: 2, marginLeft: '2%' }}
+            text={
+              isLoading
+                ? 'Carregando...'
+                : `Exibindo ${response?.current_page} de ${Number(
+                    response?.links.length
+                  )}`
+            }
+            color={Colors.gray70}
+          />
         </div>
-        <SmallMediumText
-          bold={false}
-          style={{ lineHeight: 2, marginLeft: '2%' }}
-          text={
-            isLoading
-              ? 'Carregando...'
-              : `Exibindo ${response?.current_page} de ${
-                  Number(response?.links.length) - 2
-                }`
-          }
-          color={Colors.gray70}
-        />
-      </div>
+      ) : null}
     </div>
   );
 }
---
+++ src/components/ModalError/index.tsx
@@ -0,0 +1,56 @@
+import { useRouter } from 'next/navigation';
+import Modal from 'react-bootstrap/Modal';
+
+import { Button } from 'components/Button';
+import { LitteText, SmallMediumText } from 'components/Text';
+
+import styles from './modalerror.module.css';
+
+import { Strings } from 'assets/Strings';
+import { Colors } from 'configs/Colors_default';
+
+type Props = {
+  message: string;
+  show: boolean;
+  onHide: () => void;
+};
+
+export default function ModalError({ message, onHide, ...props }: Props) {
+  const router = useRouter();
+
+  function goBack() {
+    onHide();
+    router.back();
+  }
+
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+      className={styles.headSuccess}
+    >
+      <div className={styles.headerModalSuccess}>
+        <SmallMediumText
+          text={Strings.confirmation}
+          bold={true}
+          color={Colors.gray90}
+          style={{ lineHeight: 0 }}
+        />
+      </div>
+      <Modal.Body className={styles.modalSuccess}>
+        <LitteText text={message} color={Colors.gray90} bold={false} />
+      </Modal.Body>
+      <div className={styles.footer}>
+        <Button
+          onClick={() => {
+            goBack();
+          }}
+          title={Strings.close}
+          type="secondary"
+        />
+      </div>
+    </Modal>
+  );
+}
