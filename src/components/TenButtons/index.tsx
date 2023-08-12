39fa8789d521443506ab2b36254bcccb175cba0c
Danilo Alves <33788984+danilofas@users.noreply.github.com>
2023-08-12 01:22:48 -0300

    ajuste escala de consultas

--- src/components/TenButtons/index.tsx
+++ src/components/TenButtons/index.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import React from 'react';
+
 import styles from './tenbuttons.module.css';
 
 import { addDays, format } from 'date-fns';
@@ -6,12 +10,27 @@ import ptBR from 'date-fns/locale/pt-BR';
 function TenButtons() {
   const renderButtons = () => {
     const buttons = [];
+    const [selectedDate, setSelectedDate] = React.useState<any>('');
 
     for (let i = 0; i < 10; i++) {
       const currentDate = addDays(new Date(), i);
 
       buttons.push(
-        <button key={i} className={styles.btnDateSchedule}>
+        <button
+          key={i}
+          className={styles.btnDateSchedule}
+          onClick={() => setSelectedDate(format(currentDate, 'yyyy-MM-dd'))}
+          style={{
+            backgroundColor:
+              selectedDate === format(currentDate, 'yyyy-MM-dd')
+                ? '#A1E2A5'
+                : '#F2F2F2',
+            color:
+              selectedDate === format(currentDate, 'yyyy-MM-dd')
+                ? '#fff'
+                : '#000'
+          }}
+        >
           <p
             style={{
               fontSize: '11px',
